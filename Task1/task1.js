da0c4ce8bcdaa4079cd484732ce3810722af4528
Olena1509 <elenaceniceva@gmail.com>
2024-10-15 11:19:17 +0200

    add Task1

---
+++ Task1/task1.js
@@ -0,0 +1,42 @@
+var a = 15;
+var vari = 7;
+var lex = 27;
+var b = 'text3';
+var fancy = 'property';
+let c = 35;
+let firstWeekDay = 'Monday';
+let secondWeekDay = 'Tuesday';
+let dkfj = 'FFFF123';
+let $user = 49;
+const e = 'elephant';
+const d = 'dom';
+const leo = 63;
+const h = 0;
+const user2 = 'Elena';
+
+console.log(a, vari, lex, b, fancy, c, firstWeekDay, secondWeekDay, 
+    dkfj, $user, e, d, leo, h, user2);
+document.write(a, vari, lex, b, fancy, c, firstWeekDay,
+     secondWeekDay, dkfj, $user, e, d, leo, h, user2);
+
+     alert(user2);
+     confirm(firstWeekDay);
+     prompt(a);
+
+     var text1='var використовувався для оголошення змінної,де будуть зберігаться якісь дані';
+     let text2='let це як var але новий і кращий спосіб усуває проблеми var';
+     const text3='const використовується для оголошення константи тобто незмінюваної змінної';
+     let text4= 'Декларація це оголошення змінної тобто зробили коробку';
+     let text5='Ініціалізація це надання їй значення тобто шось у коробку поклали';
+     document.write('<hr>');
+     document.write(text1);
+     document.write('<hr>');
+     document.write(text2);
+     document.write('<hr>');
+     document.write(text3);
+     document.write('<hr>');
+     document.write(text4);
+     document.write('<hr>');
+     document.write(text5);
+     
+
