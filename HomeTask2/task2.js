9adca03e6bf3f695353a7a405401aa27ebe02eb4
Olena1509 <elenaceniceva@gmail.com>
2024-10-15 11:20:09 +0200

    add Task2

---
+++ HomeTask2/task2.js
@@ -0,0 +1,115 @@
+// let a1 = 43;  //Number
+// let a2 = 'Hello JS'; // String
+// let a3 = true; // boolean( true | false)
+// let a4 = null; // null
+// let a5 = underfined; // underfined
+// let a6 = Symbol('id'); // Symbol
+// let a7 = BigInt(1234567890123456789012345); // Big int
+// let a8 = {name: 'John', age: 33 } // Object
+
+
+// second task
+let b1 = 5 + 2;
+document.write(b1);
+document.write('<hr></hr>');
+
+
+let b2 = b1 + 33;
+document.write(b2);
+document.write('<hr></hr>');
+
+let b3 = b2 - 10;
+document.write(b3);
+document.write('<hr></hr>');
+
+let b4 = b3 - 1.555;
+document.write(b4);
+document.write('<hr></hr>');
+
+let b5 = b2 * 3;
+document.write(b5);
+document.write('<hr></hr>');
+
+let b6 = b5 * 0.5;
+document.write(b6);
+document.write('<hr></hr>');
+
+let b7 = b2 / 10;
+document.write(b7);
+document.write('<hr></hr>');
+
+let b8 = b5 / 2.5;
+document.write(b8);
+document.write('<hr></hr>');
+
+let b9 = b6 % 7;
+document.write(b9);
+document.write('<hr></hr>');
+
+let b10 = b7 % 2;
+document.write(b10);
+document.write('<hr></hr>');
+
+//third task
+let c1 = '123';
+
+let c2 = Number(c1);
+console.log(c1, c2);
+
+let c3 = true;
+
+let c4 = Number(c3);
+console.log(c3, c4);
+
+let c5 = Boolean(c1);
+console.log(c1, c5);
+
+let c6 = '';
+let c7 = Boolean(c6);
+console.log(c6, c7);
+
+let c8 = 55;
+let c9 = String(c8);
+console.log(c8, c9);
+
+let c10 = true;
+let c11 = String(c10);
+console.log(c10, c11);
+
+//task 4
+let d1 = 6 + 6;
+document.write(d1);
+document.write('<hr>');
+
+d1 += 10
+document.write(d1);
+document.write('<hr>');
+
+let d2 = ++d1;
+document.write(d2);
+document.write('<hr>');
+
+d2 -= 5;
+document.write(d2);
+document.write('<hr>');
+
+d2 = d1++;
+document.write(d2);
+document.write('<hr>');
+
+let d3 = 5 ** 3;
+document.write(d3);
+document.write('<hr>');
+
+let d4 = 10 % 3;
+document.write(d4);
+document.write('<hr>');
+
+
+
+
+
+
+
+
+
